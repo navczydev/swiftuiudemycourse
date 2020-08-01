fd4465c10dd92aee4c3ecd9fc71b7fc75125af22
Nav Singh <navsingh191@gmail.com>
2020-07-31 21:57:46 -0400

    intial commit

---
+++ SwiftUIUdemy/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIUdemy
+//
+//  Created by NAVJOT SINGH on 2020-07-31.
+//  Copyright © 2020 NAVJOT SINGH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, udemy SwiftUI course!")
+            .foregroundColor(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
